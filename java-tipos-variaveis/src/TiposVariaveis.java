45a22331681e16783573072c6461430c52f891d0
Luis-Gustavo-S-Oliveira <luisgu6666@gmail.com>
2025-03-20 16:59:30 -0300

    segundo commit

--- java-tipos-variaveis/src/TiposVariaveis.java
+++ java-tipos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,25 @@
 public class TiposVariaveis {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       //tipos primitivos int,btye,short,long,float,double,boolean,char
+       // classe string representa  texto na aplicaçaõ
+
+        String MeuNome = "Luis Gustavo";
+
+        double salarioMinimo = 2500.50; // o "." que representa a virgula no java
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;// exemoplo de cast
+
+        int numero = 3;
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14; //o final junto com a expressão de nome fazem com que o valor de pi seja uma constante
+
+        
+        
+
     }
 }

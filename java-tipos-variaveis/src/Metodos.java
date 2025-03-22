101baf50f47e54ed13057a03e2b0a17e84ac398c
Luis-Gustavo-S-Oliveira <luisgu6666@gmail.com>
2025-03-21 22:05:48 -0300

    5 commit

---
+++ java-tipos-variaveis/src/Metodos.java
@@ -0,0 +1,28 @@
+public class Metodos{
+int num1 = 1;
+int num2 = 3;
+    public double  somar (int num1, int num2){
+        // 
+        
+            return somar(num1, num2);
+    }
+
+    public void imprimir (String texto){
+        // nao precisa de return, pois nao vai retornar nenhum valor
+    }
+
+    public double  dividir (int dividendo,int divisor) throws Exception {
+        // o throws Exception quer dizer que podemos dispara um exceção
+            return dividir(dividendo, divisor);
+
+    }
+
+    private void metodoPrivado(){}
+    // esse metodo nao vai estar visivel para outras classes 
+
+    public boolean  validar (){
+            return false;
+
+    }
+    
+}

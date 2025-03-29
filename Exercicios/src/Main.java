3511d8c5ff9d20ee8a6d843ca304fd1ec350c0cf
Luis-Gustavo-S-Oliveira <luisgu6666@gmail.com>
2025-03-29 16:11:36 -0300

    exercicios metodos 2

--- Exercicios/src/Main.java
+++ Exercicios/src/Main.java
@@ -15,5 +15,28 @@ public class Main {
         Emprestimo.valorEmprestimo(1000, Emprestimo.quantidadeParcela2());
         Emprestimo.valorEmprestimo(1000, Emprestimo.quantidadeParcela3());
         Emprestimo.valorEmprestimo(1000, 5);
+
+
+        //sobrecarga
+        System.out.println("Exercício quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
+
+
+        // Retornos
+        System.out.println("Exercício retornos");
+
+        double areaQuadrado = ReturnQuadrilatero.area(3);
+        System.out.println("Área do quadrado:" + areaQuadrado);
+
+        double areaRetangulo = ReturnQuadrilatero.area(5,5);
+        System.out.println("Área do retângulo:" + areaRetangulo);
+
+        double areaTrapezio = ReturnQuadrilatero.area(7,8,9);
+        System.out.println("Área do trapézio:" + areaTrapezio);
+
+
     }
 }

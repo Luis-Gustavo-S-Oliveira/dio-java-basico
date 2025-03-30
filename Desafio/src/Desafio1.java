d54e592e7a7ba49ee78a9f3a0c11e89b42f17190
Luis-Gustavo-S-Oliveira <luisgu6666@gmail.com>
2025-03-30 13:07:01 -0300

    Desafio 1

---
+++ Desafio/src/Desafio1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; 
+    
+public class Desafio1 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int entrada = leitor.nextInt();
+        int mesada = 50;
+          
+        int valorTotal = mesada * entrada;
+          
+         System.out.println( valorTotal );
+        
+        
+        // TODO: Crie a condição necessária para que o sobrinho saiba quanto dinheiro irá juntar nos meses da entrada
+
+    }
+}

0d23e7ef87e1c1f9f41653628aa8f32cc17b8871
Luis-Gustavo-S-Oliveira <luisgu6666@gmail.com>
2025-08-05 18:18:16 -0300

    exercicio 2

---
+++ Desafio_easy/src/Desafio2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Desafio2 {
+    public static void main(String[] args) {
+    
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.println("escolho 1 numero inteiro ");
+
+    int numero1 = scanner.nextInt();
+
+    System.out.println("escolho outro numero inteiro maior que o anterior ");
+
+    int numero2 = scanner.nextInt();
+
+    while (numero2 <= numero1) {
+        System.out.println("Escolha outro numero maior que o primeiro.");
+        numero2 = scanner.nextInt();
+    }  
+
+    System.out.println("escolho um numero entre impar ou par ");
+    String imparoupar = scanner.next();
+
+    if (imparoupar.equalsIgnoreCase("impar")) {
+        for (int i = numero1; i <= numero2; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }     
+    } else if (imparoupar.equalsIgnoreCase("par")) {
+        for (int i = numero1; i <= numero2; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    } else {
+        System.out.println("Opção inválida. Escolha 'impar' ou 'par'.");
+    
+    }
+
+    
+}}
